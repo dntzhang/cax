b52de56092f66b6d6e5df2bcd44f2a9679ed0856
张磊 <kmdjs@qq.com>
2015-05-23 19:03:45 +0800

    使用最新的observejs

--- are.js
+++ are.js
@@ -228,10 +228,12 @@
             return result;
         },
         "_setCursor": function (obj, type) {
-            if (obj.parent instanceof ARE.Stage) {
-                obj.parent.setCursor(type);
-            } else {
-                this._setCursor(obj.parent, type);
+            if (obj) {
+                if (obj.parent instanceof ARE.Stage) {
+                    obj.parent.setCursor(type);
+                } else {
+                    this._setCursor(obj.parent, type);
+                }
             }
         },
         "clone": function () {
@@ -477,12 +479,10 @@
             this._super();
             this.element = typeof selector == "string" ? document.querySelector(selector) : selector;
             var element = this.element;
-            var observer = ARE.Observable.watch(this, ["x", "y", "scaleX", "scaleY", "perspective", "rotation", "skewX", "skewY", "regX", "regY"]);
-            var self = this;
-            observer.propertyChangedHandler = function () {
-                var mtx = self._matrix.identity().appendTransform(self.x, self.y, self.scaleX, self.scaleY, self.rotation, self.skewX, self.skewY, self.regX, self.regY);
-                self.element.style.transform = self.element.style.msTransform = self.element.style.OTransform = self.element.style.MozTransform = self.element.style.webkitTransform = "matrix(" + mtx.a + "," + mtx.b + "," + mtx.c + "," + mtx.d + "," + mtx.tx + "," + mtx.ty + ")";
-            };
+            var observer = ARE.Observe(this, ["x", "y", "scaleX", "scaleY", "perspective", "rotation", "skewX", "skewY", "regX", "regY"],function () {
+                var mtx = this._matrix.identity().appendTransform(this.x, this.y, this.scaleX, this.scaleY, this.rotation, this.skewX, this.skewY, this.regX, this.regY);
+                this.element.style.transform = this.element.style.msTransform = this.element.style.OTransform = this.element.style.MozTransform = this.element.style.webkitTransform = "matrix(" + mtx.a + "," + mtx.b + "," + mtx.c + "," + mtx.d + "," + mtx.tx + "," + mtx.ty + ")";
+            });
             delete this.visible;
             Object.defineProperty(this, "visible", {
                 set: function (value) {
@@ -2223,99 +2223,165 @@
 
     //end-------------------ARE.Loader---------------------end
 
-    //begin-------------------ARE.Observable---------------------begin
+    //begin-------------------ARE.Observe---------------------begin
 
-    ARE.Observable = Class.extend({
-        "statics": {
-            "ctor": function () {
-                this.methods = ["concat", "every", "filter", "forEach", "indexOf", "join", "lastIndexOf", "map", "pop", "push", "reduce", "reduceRight", "reverse", "shift", "slice", "some", "sort", "splice", "unshift", "valueOf"],
-                this.triggerStr = ["concat", "pop", "push", "reverse", "shift", "sort", "splice", "unshift"].join(",");
-            },
-            "type": function (obj) {
-                var typeStr = Object.prototype.toString.call(obj).split(" ")[1];
-                return typeStr.substr(0, typeStr.length - 1).toLowerCase();
-            },
-            "isArray": function (obj) {
-                return this.type(obj) == "array";
-            },
-            "isInArray": function (arr, item) {
-                for (var i = arr.length; --i > -1;) {
-                    if (item === arr[i]) return true;
+    /* observejs --- By dnt http://kmdjs.github.io/
+   * Github: https://github.com/kmdjs/observejs
+   * MIT Licensed.
+   */
+    ; (function () {
+        var observe = function (target, arr, callback) {
+            var _observe = function (target, arr, callback) {
+                if (!target.$observer) target.$observer = this;
+                var $observer = target.$observer;
+                var eventPropArr = [];
+                if (observe.isArray(target)) {
+                    $observer.mock(target);
                 }
-                return false;
-            },
-            "isFunction": function (obj) {
-                return this.type(obj) == "function";
-            },
-            "watch": function (target, arr) {
-                return new this(target, arr);
-            }
-        },
-        "ctor": function (target, arr) {
-            for (var prop in target) {
-                if (target.hasOwnProperty(prop)) {
-                    if (arr && ARE.Observable.isInArray(arr, prop) || !arr) {
-                        this.watch(target, prop);
-                    }
-                }
-            }
-            if (target.change) throw "naming conflicts£¡observable will extend 'change' method to your object .";
-            var self = this;
-            target.change = function (fn) {
-                self.propertyChangedHandler = fn;
-            };
-        },
-        "onPropertyChanged": function (prop, value) {
-            this.propertyChangedHandler && this.propertyChangedHandler(prop, value);
-        },
-        "mock": function (target) {
-            var self = this;
-            ARE.Observable.methods.forEach(function (item) {
-                target[item] = function () {
-                    var result = Array.prototype[item].apply(this, Array.prototype.slice.call(arguments));
-                    for (var cprop in this) {
-                        if (this.hasOwnProperty(cprop) && cprop != "_super" && !ARE.Observable.isFunction(this[cprop])) {
-                            self.watch(this, cprop);
+                for (var prop in target) {
+                    if (target.hasOwnProperty(prop)) {
+                        if (callback) {
+                            if (observe.isArray(arr) && observe.isInArray(arr, prop)) {
+                                eventPropArr.push(prop);
+                                $observer.watch(target, prop);
+                            } else if (observe.isString(arr) && prop == arr) {
+                                eventPropArr.push(prop);
+                                $observer.watch(target, prop);
+                            }
+                        } else {
+                            eventPropArr.push(prop);
+                            $observer.watch(target, prop);
                         }
                     }
-                    if (new RegExp("\\b" + item + "\\b").test(ARE.Observable.triggerStr)) {
-                        self.onPropertyChanged("array", item);
-                    }
-                    return result;
-                };
-            });
-        },
-        "watch": function (target, prop) {
-            if (prop.substr(0, 2) == "__") return;
-            var self = this;
-            if (ARE.Observable.isFunction(target[prop])) return;
-            var currentValue = target["__" + prop] = target[prop];
-            Object.defineProperty(target, prop, {
-                get: function () {
-                    return this["__" + prop];
-                },
-                set: function (value) {
-                    this["__" + prop] = value;
-                    self.onPropertyChanged(prop, value);
                 }
-            });
-            if (ARE.Observable.isArray(target)) {
-                this.mock(target);
+                $observer.target = target;
+                if (!$observer.propertyChangedHandler) $observer.propertyChangedHandler = [];
+                var propChanged = callback ? callback : arr;
+                $observer.propertyChangedHandler.push({ all: !callback, propChanged: propChanged, eventPropArr: eventPropArr });
             }
-            if (typeof currentValue == "object") {
-                if (ARE.Observable.isArray(currentValue)) {
-                    this.mock(currentValue);
-                }
-                for (var cprop in currentValue) {
-                    if (currentValue.hasOwnProperty(cprop) && cprop != "_super") {
-                        this.watch(currentValue, cprop);
+            _observe.prototype = {
+                "onPropertyChanged": function (prop, value, oldValue, target, path) {
+                    if (value !== oldValue && this.propertyChangedHandler) {
+                        var rootName = observe._getRootName(prop, path);
+                        for (var i = 0, len = this.propertyChangedHandler.length; i < len; i++) {
+                            var handler = this.propertyChangedHandler[i];
+                            if (handler.all || observe.isInArray(handler.eventPropArr, rootName) || rootName.indexOf("Array-") === 0) {
+                                handler.propChanged.call(this.target, prop, value, oldValue, path);
+                            }
+                        }
+                    }
+                    if (prop.indexOf("Array-") !== 0 && typeof value === "object") {
+                        this.watch(target, prop, target.$observeProps.$observerPath);
+                    }
+                },
+                "mock": function (target) {
+                    var self = this;
+                    observe.methods.forEach(function (item) {
+                        target[item] = function () {
+                            var old = Array.prototype.slice.call(this, 0);
+                            var result = Array.prototype[item].apply(this, Array.prototype.slice.call(arguments));
+                            if (new RegExp("\\b" + item + "\\b").test(observe.triggerStr)) {
+                                for (var cprop in this) {
+                                    if (this.hasOwnProperty(cprop) && !observe.isFunction(this[cprop])) {
+                                        self.watch(this, cprop, this.$observeProps.$observerPath);
+                                    }
+                                }
+                                //todo
+                                self.onPropertyChanged("Array-" + item, this, old, this, this.$observeProps.$observerPath);
+                            }
+                            return result;
+                        };
+                    });
+                },
+                "watch": function (target, prop, path) {
+                    if (prop === "$observeProps" || prop === "$observer") return;
+                    if (observe.isFunction(target[prop])) return;
+                    if (!target.$observeProps) target.$observeProps = {};
+                    if (path !== undefined) {
+                        target.$observeProps.$observerPath = path;
+                    } else {
+                        target.$observeProps.$observerPath = "#";
+                    }
+                    var self = this;
+                    var currentValue = target.$observeProps[prop] = target[prop];
+                    Object.defineProperty(target, prop, {
+                        get: function () {
+                            return this.$observeProps[prop];
+                        },
+                        set: function (value) {
+                            var old = this.$observeProps[prop];
+                            this.$observeProps[prop] = value;
+                            self.onPropertyChanged(prop, value, old, this, target.$observeProps.$observerPath);
+                        }
+                    });
+                    if (typeof currentValue == "object") {
+                        if (observe.isArray(currentValue)) {
+                            this.mock(currentValue);
+                        }
+                        for (var cprop in currentValue) {
+                            if (currentValue.hasOwnProperty(cprop)) {
+                                this.watch(currentValue, cprop, target.$observeProps.$observerPath + "-" + prop);
+                            }
+                        }
                     }
                 }
+            }
+            return new _observe(target, arr, callback)
+        }
+        observe.methods = ["concat", "every", "filter", "forEach", "indexOf", "join", "lastIndexOf", "map", "pop", "push", "reduce", "reduceRight", "reverse", "shift", "slice", "some", "sort", "splice", "unshift", "toLocaleString", "toString", "size"]
+        observe.triggerStr = ["concat", "pop", "push", "reverse", "shift", "sort", "splice", "unshift", "size"].join(",")
+        observe.isArray = function (obj) {
+            return Object.prototype.toString.call(obj) === '[object Array]';
+        }
+        observe.isString = function (obj) {
+            return typeof obj === "string";
+        }
+        observe.isInArray = function (arr, item) {
+            for (var i = arr.length; --i > -1;) {
+                if (item === arr[i]) return true;
+            }
+            return false;
+        }
+        observe.isFunction = function (obj) {
+            return Object.prototype.toString.call(obj) == '[object Function]';
+        }
+        observe.twoWay = function (objA, aProp, objB, bProp) {
+            if (typeof objA[aProp] === "object" && typeof objB[bProp] === "object") {
+                observe(objA, aProp, function (name, value) {
+                    objB[bProp] = this[aProp];
+                })
+                observe(objB, bProp, function (name, value) {
+                    objA[aProp] = this[bProp];
+                })
+            } else {
+                observe(objA, aProp, function (name, value) {
+                    objB[bProp] = value;
+                })
+                observe(objB, bProp, function (name, value) {
+                    objA[aProp] = value;
+                })
             }
         }
-    });
+        observe._getRootName = function (prop, path) {
+            if (path === "#") {
+                return prop;
+            }
+            return path.split("-")[1];
+        }
 
-    //end-------------------ARE.Observable---------------------end
+        observe.add = function (obj, prop, value) {
+            obj[prop] = value;
+            var $observer = obj.$observer;
+            $observer.watch(obj, prop);
+        }
+        Array.prototype.size = function (length) {
+            this.length = length;
+        }
+
+         ARE.Observe = observe;
+    })();
+
+    //end-------------------ARE.Observe---------------------end
 
     //begin-------------------ARE.RAF---------------------begin
 
--- src/display/dom-element.js
+++ src/display/dom-element.js
@@ -1,20 +1,15 @@
-
-//begin-------------------ARE.DomElement---------------------begin
-
 ARE.DomElement = ARE.DisplayObject.extend({
-    "ctor": function(selector) {
+    "ctor": function (selector) {
         this._super();
         this.element = typeof selector == "string" ? document.querySelector(selector) : selector;
         var element = this.element;
-        var observer = ARE.Observable.watch(this, ["x", "y", "scaleX", "scaleY", "perspective", "rotation", "skewX", "skewY", "regX", "regY"]);
-        var self = this;
-        observer.propertyChangedHandler = function() {
-            var mtx = self._matrix.identity().appendTransform(self.x, self.y, self.scaleX, self.scaleY, self.rotation, self.skewX, self.skewY, self.regX, self.regY);
-            self.element.style.transform = self.element.style.msTransform = self.element.style.OTransform = self.element.style.MozTransform = self.element.style.webkitTransform = "matrix(" + mtx.a + "," + mtx.b + "," + mtx.c + "," + mtx.d + "," + mtx.tx + "," + mtx.ty + ")";
-        };
+        var observer = ARE.Observe(this, ["x", "y", "scaleX", "scaleY", "perspective", "rotation", "skewX", "skewY", "regX", "regY"], function () {
+            var mtx = this._matrix.identity().appendTransform(this.x, this.y, this.scaleX, this.scaleY, this.rotation, this.skewX, this.skewY, this.regX, this.regY);
+            this.element.style.transform = this.element.style.msTransform = this.element.style.OTransform = this.element.style.MozTransform = this.element.style.webkitTransform = "matrix(" + mtx.a + "," + mtx.b + "," + mtx.c + "," + mtx.d + "," + mtx.tx + "," + mtx.ty + ")";
+        });
         delete this.visible;
         Object.defineProperty(this, "visible", {
-            set: function(value) {
+            set: function (value) {
                 this._visible = value;
                 if (this._visible) {
                     this.element.style.visibility = "visible";
@@ -22,17 +17,17 @@ ARE.DomElement = ARE.DisplayObject.extend({
                     this.element.style.visibility = "hidden";
                 }
             },
-            get: function() {
+            get: function () {
                 return this._visible;
             }
         });
         delete this.alpha;
         Object.defineProperty(this, "alpha", {
-            set: function(value) {
+            set: function (value) {
                 this._opacity = value;
                 this.element.style.opacity = value;
             },
-            get: function() {
+            get: function () {
                 return this._opacity;
             }
         });
@@ -41,9 +36,7 @@ ARE.DomElement = ARE.DisplayObject.extend({
         this.element.style.visibility = "hidden";
         this.element.style.position = "absolute";
     },
-    "isVisible": function() {
+    "isVisible": function () {
         return !!(this.visible && this.alpha > 0 && this.scaleX != 0 && this.scaleY != 0);
     }
 });
-
-//end-------------------ARE.DomElement---------------------end
