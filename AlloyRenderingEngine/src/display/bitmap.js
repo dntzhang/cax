2f5e0b7b3a7fa8a15c9136d811e0e2115d7c0ad0
张磊 <kmdjs@qq.com>
2015-08-30 08:39:06 +0800

    修复clone的bug

--- AlloyRenderingEngine/src/display/bitmap.js
+++ AlloyRenderingEngine/src/display/bitmap.js
@@ -4,6 +4,18 @@
 ARE.Bitmap = ARE.DisplayObject.extend({
     "ctor": function(img) {
         this._super();
+        Object.defineProperty(this, "rect", {
+            get: function () {
+                return this["__rect"];
+            },
+            set: function (value) {
+                this["__rect"] = value;
+                this.width = value[2];
+                this.height = value[3];
+                this.regX = value[2] * this.originX;
+                this.regY = value[3] * this.originY;
+            }
+        });
         if (arguments.length === 0) return;
         if (typeof img == "string") {
             this._initWithSrc(img);
@@ -20,12 +32,8 @@ ARE.Bitmap = ARE.DisplayObject.extend({
             this.textureReady = false;
             this.img = document.createElement("img");
             this.img.crossOrigin = "Anonymous";
-            this.img.onload = function() {
+            this.img.onload = function () {
                 if (!self.rect) self.rect = [0, 0, self.img.width, self.img.height];
-                self.width = self.rect[2];
-                self.height = self.rect[3];
-                self.regX = self.width * self.originX;
-                self.regY = self.height * self.originY;
                 ARE.Bitmap[img] = self.img;
                 self.textureReady = true;
                 self.imageLoadHandle && self.imageLoadHandle();
@@ -39,18 +47,6 @@ ARE.Bitmap = ARE.DisplayObject.extend({
         this.img = img;
         this.width = img.width;
         this.height = img.height;
-        Object.defineProperty(this, "rect", {
-            get: function() {
-                return this["__rect"];
-            },
-            set: function(value) {
-                this["__rect"] = value;
-                this.width = value[2];
-                this.height = value[3];
-                this.regX = value[2] * this.originX;
-                this.regY = value[3] * this.originY;
-            }
-        });
         this.rect = [0, 0, img.width, img.height];
     },
     "useImage": function(img) {
@@ -64,7 +60,8 @@ ARE.Bitmap = ARE.DisplayObject.extend({
     "onImageLoad": function(fn) {
         this.imageLoadHandle = fn;
     },
-    "clone": function() {
+    "clone": function () {
+        if (!this.textureReady) throw "texture is not ready";
         var o = new ARE.Bitmap(this.img);
         o.rect = this.rect.slice(0);
         this.cloneProps(o);
