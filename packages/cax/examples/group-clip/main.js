b1968ab4d9a2ad6ec5f8b26f60b51a61c1223107
dntzhang <dntzhang@qq.com>
2018-07-01 13:44:30 +0800

    group clip demo

---
+++ packages/cax/examples/group-clip/main.js
@@ -0,0 +1,35 @@
+import cax from '../../src/index.js'
+const stage = new cax.Stage(260, 200, '#canvasCtn')
+const group = new cax.Group()
+const bitmap = new cax.Bitmap('./wepay-diy.jpg', () => {
+    stage.update()
+})
+
+group.x = 130
+group.y = 100
+bitmap.originX = 40
+bitmap.originY = 40
+bitmap.cursor = 'pointer'
+bitmap.on('click', () => {
+    alert('微信支付')
+})
+
+const clipPath = new cax.Graphics()
+clipPath.x = 0
+clipPath.y = 0
+clipPath.arc(0, 0, 25, 0, Math.PI * 2)
+
+const text = new cax.Text('Hello Cax Clip!')
+text.x = -30
+group.add(bitmap, text)
+
+group.clip(clipPath)
+stage.add(group)
+
+let tag = true
+
+document.querySelector('#toggleBtn').addEventListener('click', () => {
+    group.clip(tag ? null : clipPath)
+    tag = !tag
+    stage.update()
+})
