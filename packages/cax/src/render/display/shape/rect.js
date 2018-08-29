24163acb70f4e8f589b0cbe1e1672817b9f79335
张磊 <kmdjs@qq.com>
2018-08-29 15:43:24 +0800

    fix rect lineWidth

--- packages/cax/src/render/display/shape/rect.js
+++ packages/cax/src/render/display/shape/rect.js
@@ -17,6 +17,9 @@ class Rect extends Shape {
 
     if (this.option.strokeStyle) {
       this.strokeStyle(this.option.strokeStyle)
+      if(typeof this.options.lineWidth === 'number'){
+        this.lineWidth(this.options.lineWidth)
+      }
       this.strokeRect(0, 0, this.width, this.height)
     }
   }
